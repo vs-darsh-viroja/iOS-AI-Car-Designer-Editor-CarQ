c254ef8a8e77195e45458751ca99249879ef2f14
vs-darsh-viroja <darsh.vsquare.team@gmail.com>
2025-09-06 14:59:43 +0530

    Initial Commit

---
+++ CarQ/CarQApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarQApp.swift
+//  CarQ
+//
+//  Created by Purvi Sancheti on 06/09/25.
+//
+
+import SwiftUI
+
+@main
+struct CarQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarQ/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CarQ
+//
+//  Created by Purvi Sancheti on 06/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
